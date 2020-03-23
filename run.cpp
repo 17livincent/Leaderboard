bb4c3b9da0de8bd72895d619d966affb86a0ae51
Vincent Li <li.vincent0@gmail.com>
2020-03-22 22:32:26 -0700

    Started to add code specifc for reactionspeedgame

--- run.cpp
+++ run.cpp
@@ -4,6 +4,9 @@
     Version 3/18/2020
 */
 #include "leaderboard.h"
+#include <time.h>
+
+typedef std::chrono::duration<double> millis_type;
 
 using namespace std;
 
@@ -17,6 +20,22 @@ int main(int argc, char** argv) {
             save()
         
     */
+
+    /* 
+        Creating a Leaderboard of size 20 for the Reaction Speed Game,
+        in which the score is a number of seconds to the thousandths,
+        and a better score is a smaller score
+    */
+
+    chrono::system_clock::time_point clock;
+
+    // Leaderboard<score type> L(size, score_priority)
+    Leaderboard<millis_type> L(20, 0);
+
+    clock = chrono::system_clock::now();
+    string name = "First";
+    chrono::duration<double> score = chrono::duration<double>(5.0);
+    L.addRanking(L.createRanking("First", score));
 
     
 
